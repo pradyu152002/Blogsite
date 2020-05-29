9e53507717a0a647014fece5b546b3f5df2a044f
Pradyumna <prdymnvch@gmail.com>
2020-05-29 20:35:37 +0530

    gitignore update,procfile added,updated port

--- app.js
+++ app.js
@@ -197,6 +197,11 @@ app.post("/userPage",function(req,res){
 });
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
 app.listen(3000, function() {
   console.log("Server running on port 3000");
 });
