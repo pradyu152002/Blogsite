c301e741c8584b6712d2b314aa729a33fae1b027
Pradyumna <prdymnvch@gmail.com>
2020-05-29 22:54:47 +0530

    updated port properly

--- app.js
+++ app.js
@@ -198,10 +198,10 @@ app.post("/userPage",function(req,res){
 
 
 let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
+// if (port == null || port == "") {
+//   port = 8000;
+// }
 
-app.listen(3000, function() {
-  console.log("Server running on port 3000");
+app.listen(port, function() {
+  console.log("Server running on heroku port");
 });
