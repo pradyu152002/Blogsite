833e0ab0141f550d03531b9f6a43d8dbd1a8565a
Pradyumna <prdymnvch@gmail.com>
2020-05-29 11:54:05 +0530

    udated admin page and added online DB

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.set("view engine", "ejs");
-mongoose.connect("mongodb://localhost:27017/blogUserDB", {
+mongoose.connect("mongodb+srv://admin-pradyumna:Predjumna@2002$@cluster0-x867a.mongodb.net/blogUserDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
